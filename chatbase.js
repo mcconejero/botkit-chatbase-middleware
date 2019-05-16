8fc7a603ee7451a1269af45678da1d406a48a62d
Mario Centeno <mariocenteno@guellcom.net>
2019-05-16 14:32:32 +0200

    version 1

---
+++ chatbase.js
@@ -0,0 +1,51 @@
+const request = require('request');
+
+function chatbase(key, platform) {
+
+logMessage = (bot, message, type) => {
+
+  // map bot as user if not specified
+  if (!message.user) {
+    message.user = message.channelData.recipient.id;
+  }
+
+  const user = message.user || message.channelData.recipient.id
+
+  request({
+    url: 'https://chatbase.com/api/message',
+    method: 'POST',
+    json: {
+      api_key: key,
+      type: type,
+      user_id: user,
+      time_stamp: new Date().getTime() / 10000,
+      platform: platform,
+      message: JSON.stringify(message.text)
+    }
+  }, (err, httpRsp, body) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+function receive(bot, message, next) {
+  // fetch, axios, req or whatever
+  logMessage(bot, message, 'user');
+  next();
+}
+
+function send(bot, message, next) {
+  // fetch, axios, req or whatever
+  logMessage(bot, message, 'agent');
+  next();
+}
+
+return {send, receive}
+}
+
+module.exports = (controller) => {
+  controller.middleware.receive.use(receive);
+  controller.middleware.send.use(send);
+}
+
