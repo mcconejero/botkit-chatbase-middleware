8dba83c53ade3b39bc9fbd5b2cbac5a83e4a821f
Mario Centeno <mariocenteno@guellcom.net>
2019-05-17 11:54:38 +0200

    first functional version released

--- chatbase.js
+++ chatbase.js
@@ -1,45 +1,68 @@
-const request = require('request');
+'use strict';
 
-function chatbase(key, platform = 'Any') {
+var request = require('request');
 
-logMessage = (bot, message, type) => {
+class ChatbaseBotkit {
 
-  // map bot as user if not specified
-  if (!message.user) {
-    message.user = message.channelData.recipient.id;
+  constructor(apiKey, platform) {
+
+    if (!platform){
+      platform = 'Unspecified platform'
+    }
+
+    let that = this;
+
+    that.platform = platform;
+    that.apiKey = apiKey;
+
+    that.logMessage = (bot, message, type) => {
+
+      // map bot as user if not specified
+      if (!message.user) {
+        message.user = message.channelData.recipient.id;
+      }
+
+      const user = message.user || message.channelData.recipient.id
+
+      request({
+        url: 'https://chatbase.com/api/message',
+        method: 'POST',
+        json: {
+          api_key: that.apiKey,
+          type: type,
+          user_id: user,
+          time_stamp: new Date().getTime() / 1000,
+          platform: that.platform,
+          message: JSON.stringify(message.text)
+        }
+      }, (err, httpRsp, body) => {
+      });
+    }
+
+    that.send = (bot, message, next) => {
+			if (message && message.type == 'message') {
+				that.logMessage(bot, message, 'user');
+			}
+			next();
+		};
+
+		// botkit middleware endpoints
+		that.receive = (bot, message, next) => {
+			if (message && message.type == 'message') {
+				that.logMessage(bot, message, 'agent');
+			}
+			next();
+		};
+  }
+}
+
+module.exports = (apiKey, platform) => {
+  if (!apiKey) {
+    throw new Error('YOU MUST SUPPLY AN API KEY TO CHATBASE-BOTKIT!');
   }
 
-  const user = message.user || message.channelData.recipient.id
+  return {
+    botkit: new ChatbaseBotkit(apiKey, platform)
+  };
+};
 
-  request({
-    url: 'https://chatbase.com/api/message',
-    method: 'POST',
-    json: {
-      api_key: key,
-      type: type,
-      user_id: user,
-      time_stamp: new Date().getTime() / 10000,
-      platform: platform,
-      message: JSON.stringify(message.text)
-    }
-  }, (err, httpRsp, body) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-}
-
-function receive(bot, message, next) {
-  logMessage(bot, message, 'user');
-  next();
-}
-
-function send(bot, message, next) {
-  logMessage(bot, message, 'agent');
-  next();
-}
-
-return {send, receive}
-}
-
-module.exports = chatbase;
